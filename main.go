d6eba0ca6f5f906c7e23d95deccc22c1a4a1e982
Loki <tymonisat@gmail.com>
2024-08-06 12:43:09 +1200

    added a favicon

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net/http" // HTTP package
 	"github.com/gin-gonic/gin" // Gin web framework
-	cors "github.com/rs/cors/wrapper/gin" //CORs middleware
+	cors "github.com/rs/cors/wrapper/gin" // CORS middleware
 	"GoGo/src/Direct" // Import the Direct package
 	"GoGo/src/config" // Import the config package
 )
@@ -15,9 +15,21 @@ func hello_world(c *gin.Context) {
 func main() {
     config.InitConfig()
     router := gin.Default()
+    
     // Configure CORS
     router.Use(cors.AllowAll())
+    
+    // Serve static files from the "assets" directory
+    router.Static("/assets", "./assets")
+    
+    // Handle the favicon.ico request
+    router.GET("/favicon.ico", func(c *gin.Context) {
+        c.File("./assets/favicon.ico")
+    })
+    
+    // Other routes
     router.GET("/", hello_world)
     router.POST("/chat", direct.Chat)
+    
     router.Run("localhost:" + config.Config.Port)
 }
